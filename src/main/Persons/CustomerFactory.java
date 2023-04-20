75ae02c0c94b5394cbe84832fd7413303a337a8b
Yangjianxiao0203 <112916147+Yangjianxiao0203@users.noreply.github.com>
2023-04-20 11:55:29 -0400

    Customer fixed

--- src/main/Persons/CustomerFactory.java
+++ src/main/Persons/CustomerFactory.java
@@ -2,14 +2,14 @@ package main.Persons;
 
 public class CustomerFactory {
     // Factory design pattern: create Client, Manager, or Person
-    public static Person createCustomer(String type) {
+    public static Person createCustomer(String type,String userName, String password, long accountNumber, String accountType) {
         if (type.equals("Client")) {
             System.out.println("Client created");
-            return new Client();
+            return new Client(userName, password, accountNumber, accountType);
         }
         if (type.equals("Manager")) {
             System.out.println("Manager created");
-            return new Manager();
+            return new Manager(userName, password, accountNumber, accountType);
         }
         System.out.println("Error, could not create the person entity");
         return null;
