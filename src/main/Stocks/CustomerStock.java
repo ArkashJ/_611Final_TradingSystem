ac7ae7538a7c76021298148aeb2aaff4b1dabf50
ArkashJ <arkjain@bu.edu>
2023-04-29 16:56:28 -0400

    customer stocks description

--- src/main/Stocks/CustomerStock.java
+++ src/main/Stocks/CustomerStock.java
@@ -1,6 +1,22 @@
 package main.Stocks;
-
 import main.Database.Database;
+
+/*
+    * CustomerStock class is used to store the stocks that the customer has bought with the following features
+    * Stores the stock name, number of stocks, and the price at which the stocks were bought.
+    * Method to calculate the profit made by the customer on the stocks.
+    * Method to set the number of stocks and the price at which the stocks were bought.
+    * Methods:
+        * getStockName() - returns the name of the stock.
+        * getStockNumber() - returns the number of stocks.
+        * getStockBoughtPrice() - returns the price at which the stocks were bought.
+        * getProfit() - returns the profit made by the customer on the stocks.
+        * setStockNumber() - sets the number of stocks.
+        * setStockBoughtPrice() - sets the price at which the stocks were bought.
+        * Constructor:
+            * CustomerStock(String stockName, int stockNumber, double stockBoughtPrice) - initializes the stockName, stockNumber, and stockBoughtPrice.
+
+ */
 
 public class CustomerStock {
     private String stockName;
