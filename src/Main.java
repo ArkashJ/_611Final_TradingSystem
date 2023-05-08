9bd74e836b8f5048cad5739e1f7495f517564262
ArkashJ <arkjain@bu.edu>
2023-05-08 10:03:25 -0400

    explaining main

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,23 @@ import javax.swing.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/*
+ * @Description: This is the main class of the program
+ * Initiator loads the data from all the txt files into a format readable by the database
+ * The Stocks are put into a table
+ * Then the main Login/Registration page is built 
+ */
+
 public class Main {
     public static void main(String[] args) throws SQLException {
-
+        
+        // Load the data from the txt files into the database
         Initiator.initiateAll(false);
-        //build the ManagerPage
 
         SwingUtilities.invokeLater(() -> {
+            // One page for registering manager
             new UserLoginRegistration().run();
+            // One page for registering client
             new UserLoginRegistration().run();
         });
     }
--- src/main/Accounts/ITrading.java
+++ src/main/Accounts/ITrading.java
@@ -1,6 +1,11 @@
 package main.Accounts;
 
 import main.Stocks.Stock;
+/*
+    * Interface for the trading account
+    * This interface will be implemented by the TradingAccount class
+    * deposit and withdraw
+ */
 
 public interface ITrading {
     public void viewAccountDetails();
